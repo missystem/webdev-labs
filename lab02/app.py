bc01e54f3ca4a69d4fa601b15bac4583fa49bdb3
missystem <misy.shi@gmail.com>
2022-04-06 20:40:06 -0500

    optional #1 is done

--- lab02/app.py
+++ lab02/app.py
@@ -88,7 +88,7 @@ def exercise4():
         user=current_user,
         search_term=search_term,
         location=location,
-        restaurant=restaurants[0]
+        restaurants=restaurants
     )
 
 @app.route('/cards/')
