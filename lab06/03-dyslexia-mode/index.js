28a69e825c722555960583b6a9e9e5f880ffac5b
missystem <misy.shi@gmail.com>
2022-05-19 18:09:38 -0500

    fixed lab06 problem

--- lab06/03-dyslexia-mode/index.js
+++ lab06/03-dyslexia-mode/index.js
@@ -7,27 +7,28 @@
 // attach the listener
 // button id: dyslexia-toggle
 const initPage = () => {
-  const toggleButton = document.querySelector("#dyslexia-toggle");
-  toggleButton.addEventListener('click', toggleDyslexiaMode);
+    const toggleButton = document.querySelector("#dyslexia-toggle");
+    toggleButton.addEventListener('click', toggleDyslexiaMode);
 
-  const dyslexic = window.localStorage.getItem("dyslexic" === "true");
-  if (dyslexic) {
-    document.body.classList.toggle("dyslexia-mode");
-    toggleButton.setAttribute('aria-pressed', 'true');
-  } else {
-    toggleButton.setAttribute('aria-pressed', 'false');
-  }
+    const dyslexic = window.localStorage.getItem("dyslexic") === "true";
+
+    if (dyslexic) {
+        document.body.classList.toggle("dyslexia-mode");
+        toggleButton.setAttribute('aria-pressed', 'true');
+    } else {
+        toggleButton.setAttribute('aria-pressed', 'false');
+    }
 
 }
 
 const toggleDyslexiaMode = ev => {
-  document.body.classList.toggle("dyslexia-mode");
-  // aria-pressed allows people to see the button is pressed
-  let pressed = ev.currentTarget.getAttribute('aria-pressed') === 'true';
-  ev.currentTarget.setAttribute('aria-pressed', String(!pressed));
-  pressed = !pressed;
-  // when the toggle button is pressed, we will check the local storage
-  window.localStorage.setItem("dyslexic", pressed);
+    document.body.classList.toggle("dyslexia-mode");
+    // aria-pressed allows people to see the button is pressed
+    let pressed = ev.currentTarget.getAttribute('aria-pressed') === 'true';
+    ev.currentTarget.setAttribute('aria-pressed', String(!pressed));
+    pressed = !pressed;
+    // when the toggle button is pressed, we will check the local storage
+    window.localStorage.setItem("dyslexic", pressed);
 }
 
 
